e939f38d6ccb323fe8de975461d1695fd74e9d63
DeibiU <dhuertasl@ucenfotec.ac.cr>
2025-09-24 00:01:05 -0600

    Configuration files update

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: './global.css' })
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.alias = {
+	'@': __dirname, // maps @ to project root
+};
+
+module.exports = withNativeWind(config, { input: './global.css', inlineRem: 16 });
