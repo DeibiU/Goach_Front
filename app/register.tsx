1df44792195bbca65bae8735bf9b7d481465a5e3
DeibiU <dhuertasl@ucenfotec.ac.cr>
2025-09-30 00:27:30 -0600

    SVG implementation

--- app/register.tsx
+++ app/register.tsx
@@ -27,20 +27,22 @@
 import { ScrollView, View } from 'react-native';
 
 import { SignUpForm } from '@/components/sign-up-form';
+import Logo from '../assets/logo-short-black.svg'
 
 /**
  *
  */
 export default function SignUpScreen() {
-	return (
-		<ScrollView
-			keyboardShouldPersistTaps="handled"
-			contentContainerClassName="sm:flex-1 items-center justify-center p-4 py-8 sm:py-4 sm:p-6 mt-safe bg-black"
-			keyboardDismissMode="interactive"
-		>
-			<View className="w-full max-w-sm">
-				<SignUpForm />
-			</View>
-		</ScrollView>
-	);
+  return (
+    <ScrollView
+      keyboardShouldPersistTaps="handled"
+      contentContainerClassName="sm:flex-1 items-center justify-center p-4 py-8 sm:py-4 sm:p-6 mt-safe bg-black"
+      keyboardDismissMode="interactive"
+    >
+      <View className="w-full max-w-sm">
+        <SignUpForm />
+      </View>
+      <Logo />
+    </ScrollView>
+  );
 }
--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,26 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname);
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
 
-config.resolver.alias = {
-	'@': __dirname, // maps @ to project root
-};
+  const { transformer, resolver } = config;
 
-module.exports = withNativeWind(config, { input: './global.css', inlineRem: 16 });
+  config.resolver.alias = {
+    '@': __dirname, // maps @ to project root
+  };
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
+  };
+
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...resolver.sourceExts, 'svg'],
+  };
+
+  return withNativeWind(config, { input: './global.css', inlineRem: 16 });
+  
+})();
