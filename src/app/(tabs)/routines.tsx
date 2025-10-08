802998e9ce71b2723197ed6a6bb5ce2b2a7f97d4
DeibiU <dhuertasl@ucenfotec.ac.cr>
2025-10-07 21:15:17 -0600

    Workout and routine v1

--- src/app/(tabs)/routines.tsx
+++ src/app/(tabs)/routines.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-import { IUser } from '../interfaces/IUser';
+import { User } from '../interfaces/types';
 
 const profile = () => {
-  const user = React.useState<IUser>();
+  const user = React.useState<User>();
   return (
     <View className="flex-1 justify-center bg-black pl-10 pt-20 ">
       <Text className="text-7xl font-bold text-blue-500">RAAAAAAH LET'S SWEAT STEEL </Text>
