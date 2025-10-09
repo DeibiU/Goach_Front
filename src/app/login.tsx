ab7c7da36713c9c679166c77487d6747f66a234f
DeibiU <dhuertasl@ucenfotec.ac.cr>
2025-10-08 23:36:56 -0600

    responsive fix

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -9,10 +9,10 @@ export default function SignInScreen() {
   return (
     <ScrollView
       keyboardShouldPersistTaps="handled"
-      contentContainerClassName="sm:flex-1 items-center justify-center p-4 py-8 sm:py-4 sm:p-6 mt-safe bg-black"
+      className="sm:flex-1 items-center justify-center p-4 py-8 sm:py-4 sm:p-6 mt-safe bg-black"
       keyboardDismissMode="interactive"
     >
-      <View className="w-full max-w-sm">
+      <View className="w-full max-w-sm bg-black">
         <SignInForm />
       </View>
     </ScrollView>
--- src/app/register.tsx
+++ src/app/register.tsx
@@ -10,7 +10,7 @@ export default function SignUpScreen() {
   return (
     <ScrollView
       keyboardShouldPersistTaps="handled"
-      contentContainerClassName="sm:flex-1 relative items-center justify-center p-4 py-8 sm:py-4 sm:p-6 mt-safe bg-black"
+      className="sm:flex-1 relative items-center justify-center p-4 py-8 sm:py-4 sm:p-6 mt-safe bg-black"
       keyboardDismissMode="interactive"
     >
       <Logo
