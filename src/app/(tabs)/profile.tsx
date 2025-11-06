b8f0218918cdcc65afc840bd462dbe99dc60fc22
DeibiU <dhuertasl@ucenfotec.ac.cr>
2025-11-06 00:25:37 -0600

    Profile icon setup + carrousel component

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,12 +1,12 @@
 import { Link } from 'expo-router';
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, Text, View } from 'react-native';
+import TraineeIcon from '../../assets/trainee-icon.svg';
 import TrainerIcon from '../../assets/trainer-icon.svg';
+import Slider from '../components/routine-slider';
+import { Routine } from '../interfaces/types';
 import { useAuth } from '../services/auth-service';
 import { useRoutine } from '../services/routine-service';
-import Slider from '../components/routine-slider';
-import { useEffect, useState } from 'react';
-import { Routine } from '../interfaces/types';
 
 const profile = () => {
   const { user, logOut } = useAuth();
@@ -24,15 +24,18 @@ const profile = () => {
     loadRoutines();
   }, [user]);
 
-  function roleIdentifier() {
-    if (user?.role) [];
-    return 'nig';
+  function isTrainer() {
+    if (user?.role == 'TRAINER') {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   return (
     <View className="flex-1 bg-black">
       <View className="items-end">
-        <View className="flex-row">
+        <View className="flex-row p-[5px]">
           <Link
             href="/../settings"
             className="text-2xl text-green-400 "
@@ -52,20 +55,26 @@ const profile = () => {
       </View>
 
       {/* info */}
-      <View className="items-center">
-        <View className="flex-row p-20 ">
+      <ScrollView className="scrollbar-hidden px-[10%] py-[10px]">
+        <View className="items-center  lg:flex-row px-[5%] pb-[50px]">
           <View className="w-[40%] min-w-[120px] max-h-[300px]">
-            <TrainerIcon height="100%" width="100%" className="stroke-blue-500 stroke-[30]" />
+            {isTrainer() && (
+              <TrainerIcon height="100%" width="100%" className="stroke-blue-500 stroke-[30]" />
+            )}
+            {!isTrainer() && (
+              <TraineeIcon height="100%" width="100%" className="stroke-blue-500 stroke-[30]" />
+            )}
           </View>
           <View className="gap-8 justify-center pl-5">
-            <Text className="text-7xl font-bold text-purple-500">{user?.name}Bepis</Text>
-            <Text className="text-2xl text-white text-wrap">Bah! Who is lmfao?</Text>
+            <Text className="text-4xl sm:text-7xl font-bold text-purple-500 text-wrap">{user?.name}</Text>
             <Text className="text-2xl text-white">{user?.email}</Text>
             <Text className="text-2xl text-white">{user?.role}</Text>
           </View>
         </View>
-      </View>
-      <Slider itemList={userRoutines} />
+        <View className="px-[5%]">
+          <Slider itemList={userRoutines} />
+        </View>
+      </ScrollView>
     </View>
   );
 };
