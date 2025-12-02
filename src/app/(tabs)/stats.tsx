f2e8d36a19572c2773a0626d50364402e1306097
DeibiU <dhuertasl@ucenfotec.ac.cr>
2025-12-02 17:26:46 -0600

    Tab bar recreation

--- src/app/(tabs)/stats.tsx
+++ src/app/(tabs)/stats.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ActivityIndicator, ScrollView } from 'react-native';
-import { useStats, Stats } from '../services/stats-service';
+import { ActivityIndicator, ScrollView, Text, View } from 'react-native';
 import { StatsChart } from '../components/statsChart';
+import { Stats, useStats } from '../services/stats-service';
+import Home from '../../assets/home.svg';
+import List from '../../assets/list.svg';
+import StatsIcon from '../../assets/stats.svg';
+import { Link } from 'expo-router';
 
 export default function StatsScreen() {
   const { getAllStats } = useStats();
@@ -34,9 +38,31 @@ export default function StatsScreen() {
   }));
 
   return (
-    <ScrollView style={{ flex: 1, backgroundColor: 'black', padding: 16 }}>
+    <View className='flex-1 bg-black'>
+    <ScrollView className='flex-1 p-16'>
       <Text className="text-white text-3xl font-bold mb-4">Estadísticas</Text>
       <StatsChart data={chartData} />
     </ScrollView>
+    <View className="w-[100%] items-end h-auto py-[10px] mb-[10px] flex-row justify-around">
+              <View className="w-[7%] max-w-[55px] max-h-[55px]">
+                <Link href="/../profile">
+                  <Home className="fill-white" fill="#3b82f6" />
+                  <Text className="text-white text-xs sm:text-md">Profile</Text>
+                </Link>
+              </View>
+              <View className="w-[7%] max-w-[55px] max-h-[55px]">
+                <Link href="/../asignations">
+                  <List className="fill-white" fill="#ffffff" />
+                  <Text className="text-white text-xs sm:text-md">Asigs.</Text>
+                </Link>
+              </View>
+              <View className="w-[7%] max-w-[55px] max-h-[55px]">
+                <Link href="/../stats">
+                  <StatsIcon className="fill-blue-500" fill="#ffffff" />
+                  <Text className="text-blue-500 text-xs sm:text-md">Stats</Text>
+                </Link>
+              </View>
+            </View>
+            </View>
   );
 }
