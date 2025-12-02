48d88398555c56c7a2be554186041e87e3940496
DeibiU <dhuertasl@ucenfotec.ac.cr>
2025-12-02 17:25:37 -0600

    Web visual arrangement fix

--- src/app/(index)/login.tsx
+++ src/app/(index)/login.tsx
@@ -1,29 +1,26 @@
-import React from 'react';
-import { ScrollView, View } from 'react-native';
 import { SignInForm } from '@/src/app/components/sign-in-form';
 import { Marquee } from '@animatereactnative/marquee';
+import React from 'react';
+import { ScrollView, View } from 'react-native';
 import Logo from '../../assets/logo.svg';
-import { AuthPage } from '../guards/AuthPage';
 
 export default function SignInScreen() {
   return (
-    <AuthPage>
-      <ScrollView
-        keyboardShouldPersistTaps="handled"
-        keyboardDismissMode="interactive"
-        className="sm:flex-1 relative p-4 py-8 sm:py-4 sm:p-6 mt-safe bg-black"
-        contentContainerStyle={{ justifyContent: 'center', alignItems: 'center' }}
-      >
-        <View className="absolute inset-0 justify-center">
-          <Marquee spacing={2} speed={1}>
-            <Logo width={200} height={200} fill="#666666" opacity={0.1} />
-          </Marquee>
-        </View>
+    <ScrollView
+      keyboardShouldPersistTaps="handled"
+      keyboardDismissMode="interactive"
+      className="sm:flex-1 relative p-4 py-8 sm:py-4 sm:p-6 mt-safe bg-black"
+      contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}
+    >
+      <View className="absolute inset-0 justify-center">
+        <Marquee spacing={2} speed={1}>
+          <Logo className="h-[60vh] w-[160vw]" fill="#4b5563" opacity={0.2} />
+        </Marquee>
+      </View>
 
-        <View className="w-full max-w-sm bg-black">
-          <SignInForm />
-        </View>
-      </ScrollView>
-    </AuthPage>
+      <View className="w-full max-w-sm bg-black">
+        <SignInForm />
+      </View>
+    </ScrollView>
   );
 }
