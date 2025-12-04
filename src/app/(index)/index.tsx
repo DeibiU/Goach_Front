2d8fa4a5283537fa84282e6c4f3bd1d50e7b607c
DeibiU <dhuertasl@ucenfotec.ac.cr>
2025-12-04 01:12:31 -0600

    Goach logos PNG for mobile use + profile shadow

--- src/app/(index)/index.tsx
+++ src/app/(index)/index.tsx
@@ -1,19 +1,22 @@
 import { Link } from 'expo-router';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+import { isAndroid, isWeb } from '../utils/platform-flags';
 
 import Logo from '../../assets/logo-short.svg';
+const GrayLogo = require('../../assets/logo-gray-short.png');
 
 const WelcomePage = () => {
   return (
   
     <View className="flex-1 relative bg-black">
       <View className="justify-center absolute pl-10 pt-[35rem] h-[130%] w-[140%]">
-        <Logo
+        {isWeb && (<Logo
           height="100%"
           width="100%"
           className="opacity-10 inset-0 absolute fill-gray-700 animate-pulse"
-        />
+        />)}
+        {isAndroid &&(<Image source={ GrayLogo } height={10} width={10} className="opacity-10 inset-0 absolute animate-pulse" />)}
       </View>
       <View className="items-start h-[85%] sm:justify-end justify-center">
         <Text className="sm:text-7xl text-4xl pl-5 sm:pb-9 pb-4 font-bold text-blue-500">
